ad1940aef94f0811beb90538382ab7cad5e4d35d
sohaibshaikh-dev <sohaibshaikh186@gmail.com>
2025-12-16 21:38:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.add("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+ 
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor:true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  //Responsive breakpoints
+  breakpoints: {
+    0:{
+        slidesPerView:1
+    },
+      768:{
+        slidesPerView:2
+    },
+      1024:{
+        slidesPerView:3
+    },
+  }
+});
